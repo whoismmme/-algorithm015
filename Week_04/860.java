d67bc21b1a19c1764bc33e2b08418ba186180338
whoismmme <wzqdad@gmail.com>
2020-10-08 15:07:59 +0800

    week4

---
+++ Week_04/860.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        if (bills == null || bills.length == 0) {
+            return false;
+        }
+        int five = 0, ten = 0;
+        for (int i = 0; i < bills.length; i++) {
+            if (bills[i] == 5) {
+                five++;
+            } else if (bills[i] == 10) {
+                if (five == 0) return false;
+                five--;
+                ten++;
+            } else {
+                if (ten >= 1 && five >= 1) {
+                    ten--;
+                    five--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
